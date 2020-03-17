75109fd068ab515da1b90ebcad434b403ba9677e
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-18 00:01:42 +0300

    Разработана программа remove_duplicates

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string sourceStr = args[0];
+                string resultStr = new string(sourceStr.Distinct().ToArray());
+                Console.Write(resultStr);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe < input string >");
+            }
+        }
+    }
+}
