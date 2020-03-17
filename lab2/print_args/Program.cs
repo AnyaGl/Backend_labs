76e04e26ec66d585139c65214ae274e2d029c8dc
AnyaGl <gladysheva_2000@inbox.ru>
2020-03-17 23:59:03 +0300

    Разработана программа print_args

---
+++ lab2/print_args/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Number of arguments: {args.Length}");
+                Console.Write("Arguments: ");
+                foreach (string arg in args)
+                {
+                    Console.Write($"{arg} ");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+
+        }
+    }
+}
